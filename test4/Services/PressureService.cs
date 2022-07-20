f40d91002bddca5e0653f8682cdbec29572d5a27
tomaszkubrycht <tomasz.kubrycht@gmail.com>
2022-07-20 20:30:15 +0200

    ostateczne

---
+++ test4/Services/PressureService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using test4.Models;
+
+namespace test4.Services
+{
+    public class PressureService
+    {
+        private readonly IMongoCollection<Pressures> _pressures;
+
+        public PressureService(Models.IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _pressures = database.GetCollection<Pressures>("Pressure");
+        }
+        public Pressures Create(Pressures pressures)
+        {
+            _pressures.InsertOne(pressures);
+            return pressures;
+        }
+
+        public IList<Pressures> Read() =>
+                _pressures.Find(pres=>true).ToList();
+
+        
+    }
+}
+
